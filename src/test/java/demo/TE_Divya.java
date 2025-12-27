8fcaf47d2a50a1c28f744aaaf1f6a30e68487ac1
Divya C L <Divya C L@DESKTOP-2PHPB8V>
2025-12-27 13:16:42 +0530

    used annotation

---
+++ src/test/java/demo/TE_Divya.java
@@ -0,0 +1,13 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class TE_Divya {
+	
+	@Test
+	public void createClass() {
+		
+		System.out.println("Hi shilpa ....created class with anotation");
+	}
+
+}
